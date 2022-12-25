d458913021bd499bdd4b2efa44ba562ac72e0367
unknown <rele.varun1@gmail.com>
2022-12-25 10:05:09 +0530

    Added Landmarks

--- src/App.js
+++ src/App.js
@@ -20,35 +20,35 @@ const App = () => {
   }
 
   return (
-    <div className="flex h-screen items-center justify-center">
+    <main className="flex h-screen items-center justify-center">
       <div id='Main' className="container mx-4 bg-dark-grayish-blue w-[340px] md:w-[32rem] rounded-lg shadow-lg px-3  ">
         
         {/* Title with advice number */}
-        <div id="title" className='my-8 text-neon-green text-xs text-center tracking-tight font-semibold'>
+        <section id="title" className='my-8 text-neon-green text-xs text-center tracking-tight font-semibold'>
           A D V I C E <span className={`mx-3 ${count === 0 ? 'hidden' : 'span'}`}># { count }</span>
-        </div>
+        </section>
 
         {/* Main advice section */}
-        <div className="advice text-2xl text-light-cyan font-manrope text-center  -mt-3">
+        <section className="advice text-2xl text-light-cyan font-manrope text-center  -mt-3">
           "{ advice }"
-        </div>
+        </section>
 
         {/* hr + pause icon */}
-        <div id="hr" className='my-7 flex justify-center'>
+        <section id="hr" className='my-7 flex justify-center'>
           <img src={divmobile} alt="divider" className='md:hidden' />
           <img src={divdesktop} alt="divider" className='hidden md:block' />
-        </div>
+        </section>
 
         {/* button of dice */}
-        <div id='dice' className='flex justify-center'>
+        <section id='dice' className='flex justify-center'>
           <button onClick={handleAdviceClick} className='bg-neon-green rounded-full p-3 w-16 h-16 -mb-8 hover:drop-shadow-glow-neon transition-all'>
             <img src={dice} alt="dice" className='mx-auto' />
           </button>
-        </div>
+        </section>
         
         
       </div>
-    </div>
+    </main>
   )
 }
 
